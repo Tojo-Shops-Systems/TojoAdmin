9fac2e3fc52a9e7e909fb76c11fa38e35228b2fb
JaretEduardo <kaiilawlietmx@gmail.com>
2025-11-30 14:49:23 -0600

    feat/frontAndServices

--- env.tsx
+++ env.tsx
@@ -1,4 +1,5 @@
 export const Env = {
   createBranch: 'http://127.0.0.1:8001/api/cloud/create-branch',
   getBranches: 'http://127.0.0.1:8001/api/cloud/getBranches',
+  registerPersonalDataBoss: 'http://127.0.0.1:8001/api/cloud/personalData',
 };
